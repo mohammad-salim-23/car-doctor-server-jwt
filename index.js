c12face603290c0288144f970df1784019e1125c
Mohammad Salim <mohammadsalim017427@gmail.com>
2024-05-05 23:46:42 -0700

    Token create

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+ const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(cors({
+    origin:['http://localhost:5173']
+}));
 app.use(express.json());
 
 
@@ -30,7 +33,16 @@ async function run() {
 
         const serviceCollection = client.db('carDoctor').collection('services');
         const bookingCollection = client.db('carDoctor').collection('bookings');
+    //   auth related api
+      app.post('/jwt',async(req,res)=>{
+        const user = req.body;
+        console.log('user for token',user);
+        const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1h'})
+        
+        res.send({token});
+      })
 
+        // services related api
         app.get('/services', async (req, res) => {
             const cursor = serviceCollection.find();
             const result = await cursor.toArray();
---
+++ jwt__steps__.js
@@ -0,0 +1,20 @@
+/**
+ * -----------------------------
+ * MAKE API SECURE
+ * 
+ * ----------------------
+ * The person who should have
+ * 
+ * concept: 
+ * 1. assign two tokens for each person(access token ,refresh token)
+ * 2.token contains:user identification(email,role,etc.). valid for shorter duration
+ * 3.Refresh token is used: to recreate an access token.
+ * 4. If refresh is invalid then logout the user
+ * 
+ */
+
+/**
+ * 1. jwt-->json web token
+ * 
+ * 
+ */
